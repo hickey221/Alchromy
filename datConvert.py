295cc44279ab2518945c80121365861036d6239d
emartindale7 <evanmartindale@gmail.com>
2018-02-01 14:34:54 -0500

    Updates to Excel Parsing

--- datConvert.py
+++ datConvert.py
@@ -14,9 +14,15 @@ TO DO: Allow for either replicate or kinetic data output instead of file output
 import pandas as pd
 import os
 
-def datConvert(thisFile):
+def datConvert(thisFile, saveToFile = False, Kinetic = False):
     myPath = os.path.basename(thisFile) # yields 'filename.ext'
     myTitle, myExt = os.path.splitext(myPath) # yields 'filename', '.ext'
-    df = pd.read_excel(thisFile,header=1) # Read in Excel file
-    df.columns = ['nm','A'] # Rename headers
-    df.to_csv(myTitle+'.dat',sep='\t',index=False) #save as .dat file 
+    header = 0 if not Kinetic else 'None' 
+    df = pd.read_excel(thisFile,header=header) # Read in Excel file 
+    df.columns = ['nm'] + ['A']*(df.shape[1]-1) # Rename headers
+    if not Kinetic and df.shape[1]>2: df = df[['nm']] + df.drop(['nm'], axis=1).mean()
+    if saveToFile:
+        df.to_csv(myTitle+'.dat',sep='\t',index=False) #save as .dat file 
+    else:
+        return df, myTitle
+    #df.to_csv(myTitle+'.dat',sep='\t',index=False) if saveToFile else return df
