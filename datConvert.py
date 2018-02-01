44d0fa4b2af7437034ae4b639cd9767a062132af
Rich Hickey <hickey.221@osu.edu>
2018-02-01 12:34:26 -0500

    Better output file names
    Now uses input file to determine output file (something.xls -> something.dat)

--- datConvert.py
+++ datConvert.py
@@ -7,8 +7,11 @@ Created on Thu Feb  1 12:04:32 2018
 @author: hickey.221
 """
 import pandas as pd
+import os
 
 def datConvert(thisFile):
+    myPath = os.path.basename(thisFile) # yields 'filename.ext'
+    myTitle, myExt = os.path.splitext(myPath) # yields 'filename', '.ext'
     df = pd.read_excel(thisFile,header=1) # Read in Excel file
     df.columns = ['nm','A'] # Rename headers
-    df.to_csv('datOutput.dat',sep='\t',index=False) #save as .dat file 
+    df.to_csv(myTitle+'.dat',sep='\t',index=False) #save as .dat file 
