ae4af37abb83777c2f2e6bd827b4c1cfdf5f1caf
hickey221 <hickey.221@osu.edu>
2019-08-02 09:36:24 -0400

    Create alch_load.py

---
+++ lib/alch_load.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  2 08:59:50 2019
+
+@author: hickey.221
+
+Input: File path
+Can process files delimited by whitespace, comma, or xlsx
+Output: Pandas dataframe
+
+"""
+
