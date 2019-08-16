bc6fd1e5b9828475c3d58ead32eaaa2cd635255c
hickey221 <hickey.221@osu.edu>
2019-08-16 17:27:15 -0400

    Move deconv to lib

--- lib/alch_deconv.py
+++ lib/alch_deconv.py
@@ -7,14 +7,18 @@ Created on Mon Apr 16 14:57:45 2018
 from scipy.optimize import curve_fit
 import numpy as np
 
-# Define curve_fit function
+
 def func(X, *params):
+    """
+    The curve fit function:
+    Vector of coefficients multiplied by vector of data points
+    """
     return np.stack(params).dot(X)
 
-def doFitting(refCols,expCol):
-    #print("reference:\n",refCols,"Experimental\n",expCol)
-    k_init = np.ones_like(list(refCols)) # All initial guesses = 1
-    myBounds = (0,np.inf) # Coefficients between 0 and +infinity
+
+def doFitting(refCols, expCol):
+    k_init = np.ones_like(list(refCols))  # All initial guesses = 1
+    myBounds = (0, np.inf)  # Coefficients between 0 and +infinity
     coeffs, pcov = curve_fit(func, refCols.T, expCol, p0=k_init, bounds=myBounds)
     # Extract error and coefficients from results
     perr = np.sqrt(np.diag(pcov))
