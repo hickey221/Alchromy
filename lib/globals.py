810da8e13fa28b056a2db14126c721783fee8c60
hickey221 <hickey.221@osu.edu>
2020-09-16 13:49:14 -0400

    Added temp and log paths

--- lib/globals.py
+++ lib/globals.py
@@ -1,2 +1,5 @@
+# Todo: Allow rewriting from a user-edited config file
 VERSION = "1.0.1"
-DEFAULT_REFERENCE_PATH = 'ref/default.dat'
+DEFAULT_REFERENCE_PATH = 'ref/hHb_full.dat'
+TEMP_DIR_PATH = 'temp/'
+LOG_DIR = 'logs/'
